161a70eaaae7f7631bb90a8d591350cd33a0f938
Precious Offodile <87039499+poffodile@users.noreply.github.com>
2025-09-25 13:02:10 +0100

    refactorec MobileNav and Navbar components to enhance navigation functionality and styling; added close button and hamburger menu icon.

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -1,7 +1,32 @@
+import { navLinks } from "@/constant/constant";
 import React from "react";
-
+import Link from "next/link";
+import { GrClose } from "react-icons/gr";
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <div>
+      {/* overlay */}
+      <div className="fixed inset-0 transform transition-all duration-500 z-[1002] bg-black opacity-70 w-full h-screen"></div>
+      {/* navlinks  */}
+      <div
+        className=" text-white fixed justify-center flex flex-col h-full transform transition-all 
+        duration-500 delay-300 w-[80%] sm:w-[60%] bg-rose-900 space-y-6 z-[1050] ">
+        {navLinks.map((link) => {
+          return (
+            <Link key={link.id} href={link.url}>
+              <p
+                className="text-white w-fit text-[20px] ml-12 border-b-[1.5px]
+               pb-1 border-b-white sm:text-[30px]">
+                {link.label}{" "}
+              </p>
+            </Link>
+          );
+        })}
+        {/* close button */}
+        <GrClose className="absolute top-[1.7rem] right-[2rem] sm:w-8 sm:h-8 w-6 h-6" />
+      </div>
+    </div>
+  );
 };
 
 export default MobileNav;
--- components/Home/Navbar/Navbar.tsx
+++ components/Home/Navbar/Navbar.tsx
@@ -2,10 +2,11 @@ import { navLinks } from "@/constant/constant";
 import Link from "next/link";
 import React from "react";
 import { FaPassport } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
   return (
-    <div className="bg-blue-950 transition-all duration-200 h-[12vh] z-[1000]">
+    <div className="bg-blue-950 transition-all duration-200 h-[12vh] z-[1000] fixed  w-full">
       <div className="flex justify-between items-center h-full w-[90%] xl:w-[80%] mx-auto">
         {/*Logo */}
         <div className="flex items-center space-x-2">
@@ -18,16 +19,29 @@ const Navbar = () => {
           </h1>
         </div>
         {/* Navigation Links */}
-        <div className=" hidden lg:flex items-center space-x-10">
+        <div className=" hidden lg:flex items-center space-x-10 ">
           {navLinks.map((link) => (
             <Link href={link.url} key={link.id}>
               <p
-                className="relative text-white text-base font-medium w-fit block 
-              after:block after:content-[''] after:absolute after:h-[3px] after:bg-yellow-300 after:w-full after:scale-x-0  after:hover:scale-x-100 after:transition duration-300 after:origin-right">
+                className="relative text-white text-xl font-medium  w-fit block
+  after:block after:content-[''] after:absolute after:h-[3px] after:bg-yellow-300 after:w-full
+  after:bottom-0 after:left-0 after:scale-x-0 after:transform after:transition-transform after:duration-300 after:origin-right
+  hover:after:scale-x-100">
                 {link.label}
               </p>
             </Link>
           ))}
+        </div>
+
+        {/* Buttons  */}
+        <div className="flex items-center space-x-4">
+          <button
+            className="md:px-12 md:py-2.5 px-8 py-2 text-black text-base bg-white hover:bg-gray-200 
+        transition-all duration-200 rounded-lg ">
+            Download Cv
+          </button>
+          {/* Burger Menu  */}
+          <RxHamburgerMenu className="w-8 h-8 cursor-pointer text-white lg:hidden" />
         </div>
       </div>
     </div>
